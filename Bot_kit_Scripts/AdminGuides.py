b0b0f17dd1675be1d575c43cac7b21d0346c9796
legex <53468757+legex@users.noreply.github.com>
2022-12-03 15:47:24 +0530

    updating adminguide command

--- Bot_kit_Scripts/AdminGuides.py
+++ Bot_kit_Scripts/AdminGuides.py
@@ -2,22 +2,21 @@ import logging
 from webex_bot.models.command import Command
 from datetime import*
 import re
-from Scraper import*
+from AdminGuide_Main import*
 
 log = logging.getLogger(__name__)
 
 # Get a free account at openweathermap.org &
 # insert API key here:
-
-class AdminGuide(Command):
+class AdminGuides(Command):
     def __init__(self):
         # Define custom command info here
         # command_keyword = what chat keyword will trigger this command to execute
         # help_message = what message is returned when user sends 'help' message
         # card = optionally send an AdaptiveCard response
         super().__init__(
-            command_keyword="admin guide" or "guide" or "Admin guide",
-            help_message="Syntax: admin guide devicetype",
+            command_keyword="admin guide" or "Admin guide",
+            help_message="Admin guide Syntax: admin guide devicetype",
             card=None,
         )
 
@@ -25,9 +24,8 @@ class AdminGuide(Command):
         # By default, command keyword will be stripped out before being passed to execute function
         # For example, If user sends "weather 12345", then message variable will be " 12345"
         # Need to strip the additional whitespace around the input:
-        admin_link=[]
         valid_input=str(message)
-        ptrn = r'([aA-zZ]{2}\d{2,3})|([r,c][aA-zZ]{3,12}.\d{2})|((roomkit).{0,5})'
+        ptrn = r'([aA-zZ]{2}\d{2,3})|([r,c][aA-zZ]{3,12}.\d{2}.{0,1})|((roomkit).{0,5})'
         match=re.search(ptrn, valid_input)
         while match!= None:
             try:
@@ -35,23 +33,19 @@ class AdminGuide(Command):
             except:
                 print("Unable to match")
             break
-        url='https://www.cisco.com/c/en/us/support/collaboration-endpoints/spark-room-kit-series/products-maintenance-guides-list.html'
+        #Since callback command will be stripped, taking rest of the input to store in a variable
+        devices=vc_unit.upper()
+        # Using the variable to extract required links
+        adg=AdminGuide_Main(devices)
+        guides_fin=adg.curated_guide()
+        serial_num=[]
 
-        if vc_unit.upper() == 'ROOM 70' or 'ROOM 55':
-            URLAccess.urlaccessed(url)
-            regx=re.compile(".+desk-room-board.+")
-            default_rm_lnk=sorted(set(list(filter(regx.match, linkcontent))))
-            regx2=re.compile(".+room-55d-70-administrator.+")
-            unitlink=sorted(set(list(filter(regx2.match, linkcontent))))
-            final_link= default_rm_lnk + unitlink
-            for endlinks in final_link:
-                admin_link.append(endlinks)
-            tshootlink=admin_link
-        else:
-            tshootlink= f"something is not write"
-        
-        response_message= ("Links for {devc} admin guides are below: \n {endl}".format(devc=vc_unit,endl='\n'.join(tshootlink)))
+        for serial in range(1,len(guides_fin)+1):
+            serial_num.append(serial)
+        output_links='''
+        '''
+        for a,b in zip(serial_num, guides_fin):
+            output_links += str(a)+' ' +b +'\n'
+        return output_links
 
-        # Define our URL, with desired parameters: ZIP code, units, and API Key
         # Message returned will be sent back to the user by bot
-        return response_message
