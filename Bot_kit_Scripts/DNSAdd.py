7ebb1eefe8c7160ca716fc7ddae59f571360de4d
legex <53468757+legex@users.noreply.github.com>
2022-12-03 12:04:39 +0530

    Update DNSAdd.py

---
+++ Bot_kit_Scripts/DNSAdd.py
@@ -0,0 +1,69 @@
+import json
+import logging
+import requests
+from webex_bot.models.command import Command
+import re
+import paramiko
+
+log = logging.getLogger(__name__)
+
+# Get a free account at openweathermap.org &
+# insert API key here:
+dict_creds_host={
+    'hostname':'10.197.193.171',
+    'username':'uclab\Administrator',
+    'password':'tActeam132'
+}
+
+class DNSAdd(Command):
+    def __init__(self):
+        # Define custom command info here
+        # command_keyword = what chat keyword will trigger this command to execute
+        # help_message = what message is returned when user sends 'help' message
+        # card = optionally send an AdaptiveCard response
+        super().__init__(
+            command_keyword="DNS" or "dns",
+            help_message="Syntax: dns add 'fqdn' 'ip'",
+            card=None,
+        )
+
+    def execute(self, message, attachment_actions, activity):
+        valid_input=str(message)
+        ptrn1 = r'(\w{3,4}([e,c]|[p,s])(\d{2}\w{2,9}))'
+        ptrn2= r'(\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3})'
+        match=re.search(ptrn1, valid_input)
+        match2=re.search(ptrn2,valid_input)
+        while (match!= None) and (match2!= None):
+            try:
+                hostad=match.group()
+                ip=match2.group()
+                break
+            except:
+                print("Unable to match")
+                break
+
+        cmd = 'powershell Add-DnsServerResourceRecordA -Name "{FQDN}" -ZoneName "uclab.local" -AllowUpdateAny -IPv4Address "{IP_address}" -CreatePtr -TimeToLive 01:00:00'.format(FQDN=hostad,IP_address=ip)
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(dict_creds_host['hostname'],username=dict_creds_host['username'],password=dict_creds_host['password'])
+        except paramiko.AuthenticationException:
+            print("Failed to connect to %s due to wrong username/password" %dict_creds_host['hostname'])
+            exit(1)
+        except Exception as e:
+            print(e.message)    
+            exit(2)
+
+        try:
+            stdin, stdout, stderr = ssh.exec_command(cmd)
+            err = ''.join(stderr.readlines())
+            out = ''.join(stdout.readlines())
+            final_output = ''
+            if (out=='') and (err==''):
+                final_output="DNS Entry Added as {FQDN}.uclab.local againts {IPV4}".format(FQDN=hostad,IPV4=ip)
+            else:
+                final_output=str(out)+str(err)
+            print(final_output)
+        except Exception as e:
+            print(e.message)
+        return final_output
